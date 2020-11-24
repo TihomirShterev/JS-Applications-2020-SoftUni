aef46aa1cc64b60af25f8c487e4a8526a58ff070
TihomirShterev <tisho_92@abv.bg>
2020-11-24 18:37:30 +0200

    Committed an already solved project by myself

---
+++ 2020-08-08-Regular/config/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCtTznzKLEfNivG9lQVjqOCfx8-BONzCbw",
+  authDomain: "examregularhw-1acb6.firebaseapp.com",
+  databaseURL: "https://examregularhw-1acb6.firebaseio.com",
+  projectId: "examregularhw-1acb6",
+  storageBucket: "examregularhw-1acb6.appspot.com",
+  messagingSenderId: "840222957104",
+  appId: "1:840222957104:web:0b7879ca7b766647fc17f5",
+  measurementId: "G-F59QT9R3ET"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
